3266ed7505fd3e09a403476d7b46cd443c34ba05
Safir MEDJEBER <safir.medjeber@gmail.com>
2022-03-23 19:03:37 +0100

    fix param

--- src/main/java/services/StatementService.java
+++ src/main/java/services/StatementService.java
@@ -11,7 +11,7 @@ public class StatementService implements BankStatementService {
 
     @Override
     public void updateAccountStatement(BankAccount account, int amount, String operation, ZonedDateTime zonedDateTime) {
-        account.accountStatements.add(new AccountStatement(ZonedDateTime.now(), amount, operation));
+        account.accountStatements.add(new AccountStatement(zonedDateTime, amount, operation));
     }
 
     @Override
